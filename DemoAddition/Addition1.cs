74984fb5a5104cbd16607769f279035731d01218
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-10-29 15:15:26 +0530

    [shravanthi] - Method to do addition

---
+++ DemoAddition/Addition1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoAddition
+{
+    public class Addition1
+    {
+        public void Add()
+        {
+            int x = 1;
+            int y = 2;
+            int z = x+y;
+            Console.WriteLine(z);
+        }
+    }
+}

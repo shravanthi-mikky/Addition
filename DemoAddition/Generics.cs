a427d1ad05094e1912dbdf5c80b652343c448ab0
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-11-12 16:26:41 +0530

    [shravanthi] - Generics Explanation

---
+++ DemoAddition/Generics.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace DemoAddition
+{
+    public class Generics
+    {
+
+        public void Comparision<T>(T num1, T num2)
+        {
+            if(num1.Equals(num2))
+            {
+                Console.WriteLine("Numbers are equal");
+            }
+            else 
+            {
+                Console.WriteLine("Numbers are not equal");
+            }
+        }
+    }
+}
--- DemoAddition/Program.cs
+++ DemoAddition/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace DemoAddition
 {
@@ -9,6 +11,35 @@ namespace DemoAddition
             Console.WriteLine("Hello World!");
             Addition1 addition1 = new Addition1();
             addition1.Add();
+
+            // calling the comparision method of generics
+            Generics generics = new Generics();
+            generics.Comparision(2.7, 2.7);
+
+            //Array, has specific length, and is type safe
+            // has specific length, only can be updated later.
+
+            int[] array = {1,2, 3};
+            array[0] = 2;
+            array[1] = 3;
+            array[2] = 4;
+            //array[3] = 5; // System.IndexOutOfRangeException: 'Index was outside the bounds of the array.'
+            Console.WriteLine(array[3]);
+
+            //Arraylist is not type safe, it takes any type of value in it bcos it takes object as input.
+            ArrayList arrayList1 = new ArrayList(10);
+
+            arrayList1.Add(100);
+            arrayList1.Add("Hello"); // accepts all types of data
+
+            //List is type safe, it takes only specific type of data.
+
+            List<int> list = new List<int>();
+            list.Add(100);
+            // list.Add(("hello");// throws an error, cannot convert string to int 
+
+
+
         }
     }
 }
